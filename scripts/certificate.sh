b331bfd94b903e1b80a1a2ed95913334e47da515
kyle-park-io <andy3638@naver.com>
2025-10-31 09:46:19 +0900

    chore: add kyle-quant certbot cli

--- scripts/certificate.sh
+++ scripts/certificate.sh
@@ -1,4 +1,7 @@
+# list certificates
+# sudo certbot certificates
 certbot certificates
 
+# expire date
 # openssl x509 -enddate -noout -in /etc/letsencrypt/live/jungho.dev/fullchain.pem
 openssl x509 -enddate -noout -in /data/letsencrypt/live/jungho.dev/fullchain.pem
--- scripts/dns-cloudflare.sh
+++ scripts/dns-cloudflare.sh
@@ -14,3 +14,20 @@ sudo certbot certonly \
   --domains jungho.dev \
   --agree-tos \
   --non-interactive
+
+sudo certbot certonly \
+  --staging \
+  --dns-cloudflare \
+  --dns-cloudflare-credentials ./ini/cloudflare.ini \
+  --email andy3638@naver.com \
+  --domains kyle-quant.xyz \
+  --agree-tos \
+  --non-interactive
+
+sudo certbot certonly \
+  --dns-cloudflare \
+  --dns-cloudflare-credentials ./ini/cloudflare.ini \
+  --email andy3638@naver.com \
+  --domains kyle-quant.xyz \
+  --agree-tos \
+  --non-interactive
